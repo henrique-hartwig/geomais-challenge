ea2145d33b05a32181c9ef9c1807df940cff97c0
henrique.hartwig <henrique2805@gmail.com>
2024-01-26 13:49:31 -0300

    Update fake API files

--- fake-api/server.js
+++ fake-api/server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('../db.json')
 const middlewares = jsonServer.defaults()
-const PORT = 3000
+const PORT = 3001
 
 server.use(middlewares)
 server.use(router)
